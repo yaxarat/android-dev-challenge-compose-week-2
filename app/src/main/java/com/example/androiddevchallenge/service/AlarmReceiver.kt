328cd2cfc4698215da49413db4af207fee95dba1
Yashar Atajan <yaxarat@users.noreply.github.com>
2021-03-06 09:11:33 -0500

    ran spotless
    ./gradlew :app:spotlessApply

--- app/src/main/java/com/example/androiddevchallenge/service/AlarmReceiver.kt
+++ app/src/main/java/com/example/androiddevchallenge/service/AlarmReceiver.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.service
 
 import android.app.NotificationManager
@@ -6,7 +21,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.core.content.ContextCompat
 
-class AlarmReceiver: BroadcastReceiver() {
+class AlarmReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         // TODO: Step 1.10 [Optional] remove toast
@@ -22,7 +37,5 @@ class AlarmReceiver: BroadcastReceiver() {
         //     context.getText(R.string.eggs_ready).toString(),
         //     context
         // )
-
     }
-
 }
